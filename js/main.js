1a25278c7a6ed3551ba376f245e885082b8ee0ce
Timur <teekirienkov@gmail.com>
2020-05-05 15:05:27 +0300

    add modal auth

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
-const cartButton = document.querySelector("#cart-button");
-const modal = document.querySelector(".modal");
-const close = document.querySelector(".close");
+const cartButton = document.querySelector("#cart-button"),
+      modal = document.querySelector(".modal"),
+      close = document.querySelector(".close");
 
 cartButton.addEventListener("click", toggleModal);
 close.addEventListener("click", toggleModal);
 
 function toggleModal() {
   modal.classList.toggle("is-open");
+}
+
+
+const buttonAuth = document.querySelector('.button-auth'),
+      modalAuth = document.querySelector('.modal-auth'),
+      closeAuth = document.querySelector('.close-auth');
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle('is-open');
+}
+
+buttonAuth.addEventListener('click', toggleModalAuth);
+
+closeAuth.addEventListener('click', toggleModalAuth);
+
+function authorized() {
+  console.log('Auth')
+}
+
+function notAuthorized() {
+  console.log('No auth')
 }
