eda15ac55797a45a0fa90057ff1ef03906d81eb0
Timur <teekirienkov@gmail.com>
2020-05-05 15:12:07 +0300

    add func

--- js/main.js
+++ js/main.js
@@ -9,23 +9,38 @@ function toggleModal() {
   modal.classList.toggle("is-open");
 }
 
+// day 1
 
 const buttonAuth = document.querySelector('.button-auth'),
       modalAuth = document.querySelector('.modal-auth'),
-      closeAuth = document.querySelector('.close-auth');
+      closeAuth = document.querySelector('.close-auth'),
+      logInForm = document.querySelector('#logInForm');
+
+let login = ''; // пустая строка для проверки
+
+if (login) {
+  authorized();
+} else {
+  notAuthorized();
+}
 
 function toggleModalAuth() {
   modalAuth.classList.toggle('is-open');
 }
-
-buttonAuth.addEventListener('click', toggleModalAuth);
-
-closeAuth.addEventListener('click', toggleModalAuth);
 
 function authorized() {
   console.log('Auth')
 }
 
 function notAuthorized() {
+
+  function logIn(event) {
+    event.preventDefault();
+    console.log(event)
+  }
+
   console.log('No auth')
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click', toggleModalAuth);
+  logInForm.addEventListener('submit', logIn);
 }
